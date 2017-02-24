936f759d622a308d04c4d52d9b4c4586639baed0
Ashwin Balaji <vashwinbalaji@gmail.com>
2017-02-24 10:19:25 +0800

    Update CircleMovement.cpp

--- CircleMovement.cpp
+++ CircleMovement.cpp
@@ -5,12 +5,10 @@ m3pi m3pi;
  
 int main() {
  
-    m3pi.printf("Hi World");
- 
+    m3pi.printf("Circle");
     wait (2.0);
     m3pi.cls();
-    
-   
+     
     m3pi.right_motor(0.5);
     
     wait(5.0);
