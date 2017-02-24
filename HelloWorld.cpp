7c081083301d9085920c5651e82ee8bb0cefab7e
Ashwin Balaji <vashwinbalaji@gmail.com>
2017-02-24 10:11:23 +0800

    Update HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,12 +1,13 @@
 #include "mbed.h"
 #include "m3pi.h"
- 
+//Necessary header files must be included
+
 m3pi m3pi;
- 
+//Initialising the object m3pi
+
 int main() {
  
-    m3pi.printf("Hi World");
- 
-    wait (2.0);
-    m3pi.stop();       
+    m3pi.printf("Hi World"); //To print on the LCD screen of the m3pi
+    wait (2.0);              //Wait for 2 seconds before executing the next set of code
+    m3pi.stop();             //To stop m3pi execution
 }
